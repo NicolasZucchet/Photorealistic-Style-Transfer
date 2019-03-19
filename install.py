4a3568be5ffb47c89d4fdbea34ac9e6cd0455b6b
victorruelle <37347753+victorruelle@users.noreply.github.com>
2019-03-19 14:39:04 +0100

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import wget
-from toolbox.path_setup import download_models
 
 os.system(sys.executable + " -m pip install -r requirements.txt")
 # image segmentation
@@ -9,8 +8,9 @@ if not os.path.exists("semsegpt"):
     os.system("git clone https://github.com/CSAILVision/semantic-segmentation-pytorch.git semsegpt")
 
 # download models for segmentation
+from toolbox.path_setup import download_models
 print("pls")
-download_models()
+download_models() # yyilds an error on my windows machine
 print("pk")
 
 
